f7522a4e1178f877b6c93b2e288023cbd5b7da24
lucimaster100 <74505266+lucimaster100@users.noreply.github.com>
2022-01-04 23:54:50 +0200

    v1.1
    added user options for grouping small or less frequent files

--- analize_partition.py
+++ analize_partition.py
@@ -4,6 +4,10 @@ import collections
 import matplotlib.pyplot as plt
 import itertools
 
+
+count_limit=int(input("Set grouping limit for file count\ncount_limit="))
+size_limit=int(input("Set grouping limit for file size(in GB)\nsize_limit="))
+
 drive=sys.argv[1]
 ndirs=0
 nfiles=0
@@ -19,19 +23,19 @@ for (root,directories,files) in os.walk(drive+":\\"):
         size[ext]+=os.path.getsize(full_fileName)
 print("directories: " + f"{ndirs}" +" files: " + f"{nfiles}")
 
-#group files that appear less than 150 times
+#group files that appear less than a number of times
 newcount={}
-for key, group in itertools.groupby(count, lambda k: 'Other files' if (count[k]<500) else k):
+for key, group in itertools.groupby(count, lambda k: 'Other files' if (count[k]<count_limit) else k):
      newcount[key] = sum([count[k] for k in list(group)])
 print("file extention count:")
-print(count)
+print(newcount)
 
-#group files less than 10GB
+#group files less than a given size in GB
 newsize={}
-for key, group in itertools.groupby(size, lambda k: 'Other files' if (size[k]<10000000000) else k):
+for key, group in itertools.groupby(size, lambda k: 'Other files' if (size[k]<1000000000*size_limit) else k):
      newsize[key] = sum([size[k] for k in list(group)])
 print("file extention size:")
-print(size)
+print(newsize)
 fig, ax = plt.subplots()
 ax.pie(newcount.values(),labels=newcount.keys())
 ax.axis=('equal')
