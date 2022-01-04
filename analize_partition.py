57f194af325b9f48f7b7ad170816db1498cfe8fc
lucimaster100 <74505266+lucimaster100@users.noreply.github.com>
2022-01-04 23:40:14 +0200

    1.0

--- analize_partition.py
+++ analize_partition.py
@@ -2,13 +2,13 @@ import sys
 import os
 import collections
 import matplotlib.pyplot as plt
+import itertools
 
 drive=sys.argv[1]
 ndirs=0
 nfiles=0
 count = collections.Counter()
 size=collections.Counter()
-
 for (root,directories,files) in os.walk(drive+":\\"):
     ndirs+=len(directories)
     nfiles+=len(files)
@@ -18,7 +18,25 @@ for (root,directories,files) in os.walk(drive+":\\"):
         count[ext]+=1
         size[ext]+=os.path.getsize(full_fileName)
 print("directories: " + f"{ndirs}" +" files: " + f"{nfiles}")
-fig1, ax1 = plt.subplots()
-ax1.pie(count.values(),labels=count.keys())
-ax1.axis=('equal')
+
+#group files that appear less than 150 times
+newcount={}
+for key, group in itertools.groupby(count, lambda k: 'Other files' if (count[k]<500) else k):
+     newcount[key] = sum([count[k] for k in list(group)])
+print("file extention count:")
+print(count)
+
+#group files less than 10GB
+newsize={}
+for key, group in itertools.groupby(size, lambda k: 'Other files' if (size[k]<10000000000) else k):
+     newsize[key] = sum([size[k] for k in list(group)])
+print("file extention size:")
+print(size)
+fig, ax = plt.subplots()
+ax.pie(newcount.values(),labels=newcount.keys())
+ax.axis=('equal')
+plt.show()
+fig, ax = plt.subplots()
+ax.pie(newsize.values(),labels=newsize.keys())
+ax.axis=('equal')
 plt.show()
